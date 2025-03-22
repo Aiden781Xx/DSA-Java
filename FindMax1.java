0e62a813f37b1a2285bcd5d7e7f829cdf5fee56f
Aiden781Xx <Shivambhardwaj750000@gmail.com>
2025-03-22 09:51:48 +0530

    Add files via upload

---
+++ FindMax1.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.util.Scanner;
+
+class FindMax1 {
+    public static int FindMax11(int[] arr) {
+        int max = arr[0];
+        for (int num : arr) {
+            if (num > max) {
+                max = num;
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the size of the array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        System.out.println("Enter the elements of the array: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("The maximum element in the array is: " + FindMax11(arr));
+
+        sc.close();
+    }
+}
